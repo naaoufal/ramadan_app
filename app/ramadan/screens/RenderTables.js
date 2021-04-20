3cd171699b370933f737d19c7f7547dcb8548635
naaoufal <naoufelbenmensour@gmail.com>
2021-04-20 08:43:48 +0000

    add files

--- app/ramadan/screens/RenderTables.js
+++ app/ramadan/screens/RenderTables.js
@@ -8,11 +8,51 @@ export class TableData extends Component () {
     constructor () {
         super()
         this.fstore = firebase.firestore().collection('tables')
+        this.state = {
+            tableArr : []
+        }
+    }
+
+    // executed after the first render only on the client side
+    componentDidMount() {
+        this.tables = this.fstore.onSnapshot(this.getCollection);
+    }
+
+    // executed before rendering, on both the server and the client side
+    componentWillUnmount(){
+        this.tables();
+    }
+
+    getData = (querySnapshot) => {
+        const userArr = [];
+        querySnapshot.forEach((res) => {
+          const { username, message, latitude, longitude } = res.data();
+          userArr.push({
+            key: res.id,
+            res,
+            username,
+            message,
+            latitude,
+            longitude
+          });
+        });
+        this.setState({
+          tableArr
+       });
     }
 
     render() {
         return (
-            <View></View>
+            <View>
+                {this.state.tableArr.map((table, i) => {
+                    return (
+                        <ListItem
+                        key = {i}
+                        title = {table.name}
+                        />
+                    )
+                })}
+            </View>
         )
     }
 }
